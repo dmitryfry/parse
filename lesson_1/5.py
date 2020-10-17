70a3e239e2fd8c39f99a15159a3323667f21f800
Dmitry Fry <im@dmitryfry.com>
2020-10-17 13:48:10 +0300

    lesson_1

--- lesson_1/5.py
+++ lesson_1/5.py
@@ -1,38 +1,67 @@
 import json
 import requests
+import pdb
+import time
 
+# {
+# "name": "имя категории",
+# "code": "Код соответсвующий категории (используется в запросах)",
+# "products": [{PRODUCT},  {PRODUCT}........] # список словарей товаров соответсвующих данной категории
+# }
 
 class Parser5ka:
     __params = {
         'records_per_page': 50,
     }
-    __headers = {
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:81.0) Gecko/20100101 Firefox/81.0',
-    }
 
-    def __init__(self, start_url):
+    def __init__(self, start_url, categories_url):
         self.start_url = start_url
+        self.categories_url = categories_url
 
-    def parse(self, url=None):
+    def categories(self, url=None):
+        if not url:
+            url = self.categories_url
+        response = requests.get(url)
+        categories: dict = response.json()
+        return categories
+
+    def parse(self, url=None, categories=None):
         if not url:
             url = self.start_url
         params = self.__params
-        while url:
+        for category in categories:
+            category_data = []
+            while url:
+                # pdb.set_trace()
+                params['categories'] = int(category['parent_group_code'])
 
-            response = requests.get(url, params=params, headers=self.__headers)
-            if params:
-                params = {}
-            data: dict = response.json()
-            url = data['next']
+                response = requests.get(url, params=params)
+                if params:
+                    params = {}
+                data: dict = response.json()
+                if data['next'] == None:
+                    print(category['parent_group_name'])
+                    print('break')
+                    break
+                elif data['next']:
+                    print(category['parent_group_name'])
+                    print(data['next'])
+                    url = data['next']
+                    category_data.append(data['results'])
+                time.sleep(0.1)
+            json_data = {
+                "name": category['parent_group_name'],
+                "code": category['parent_group_code'],
+                "products": category_data
+            }
+            self.save_to_json_file(json_data, category['parent_group_name'])
 
-            for product in data['results']:
-                self.save_to_json_file(product)
-
-    def save_to_json_file(self, product: dict):
-        with open(f'products/{product["id"]}.json', 'w', encoding='UTF-8') as file:
-            json.dump(product, file, ensure_ascii=False)
+    def save_to_json_file(self, json_data, category_name):
+        with open(f'products/{category_name}.json', 'w', encoding='UTF-8') as file:
+            json.dump(json_data, file, ensure_ascii=False)
 
 
 if __name__ == '__main__':
-    parser = Parser5ka('https://5ka.ru/api/v2/special_offers/')
-    parser.parse()
+    parser = Parser5ka('https://5ka.ru/api/v2/special_offers/', 'https://5ka.ru/api/v2/categories/')
+    parser.categories()
+    parser.parse(categories=parser.categories())
